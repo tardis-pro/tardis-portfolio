de6ec2c795ba18baac21f2e83ad5ff13aa035e98
Mandy0513 <Mandy0513@users.noreply.github.com>
2022-09-07 16:44:55 +0530

    added content and some styling

--- tardis/src/Components/OurProjects.js
+++ tardis/src/Components/OurProjects.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./OurProjects.css";
-import app from './img/app.png';
+import app from "./img/app.png";
 
 const OurProjects = () => {
   return (
@@ -22,60 +22,78 @@ const OurProjects = () => {
                 <div className="pdesc">
                   <h2>Project#1</h2>
                   <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    Deleniti porro fugiat ducimus incidunt vitae sequi, nemo
-                    aspernatur quo non, provident debitis minima a, odio nisi
-                    laborum! Numquam et omnis aperiam.iat ducimus incidunt vitae
-                    sequi, nemo aspernatur quo non, provident debitis minima a,
-                    odio nisi laborum! Numquam et omnis aperiam.
+                    <b style={{ fontSize: "20px" }}> NPhub</b> <br />
+                    Technologies used - Angular,Node, Mongo <br /> Project
+                    Description: Find the perfect preceptor &amp; speed through
+                    your paperwork process. <br /> • Developed UI screens from
+                    mockups provided. <br />• Created and integrated APIs with
+                    frontend.
+                    <br /> • Interfaced with different 3rd party services such
+                    as Stripe, PandaDocs.
                   </p>
                 </div>
               </div>
               <div className="project-item">
                 <div className="picon">
-                <img src={app} alt="app" />;
+                  <img src={app} alt="app" />;
                 </div>
                 <div className="pdesc">
                   <h2>Project#2</h2>
                   <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    Deleniti porro fugiat ducimus incidunt vitae sequi, nemo
-                    aspernatur quo non, provident debitis minima a, odio nisi
-                    laborum! Numquam et omnis aperiam.iat ducimus incidunt vitae
-                    sequi, nemo aspernatur quo non, provident debitis minima a,
-                    odio nisi laborum! Numquam et omnis aperiam.
+                    <b style={{ fontSize: "20px" }}> Welgo</b> <br />
+                    Technologies - Python,Django, AngularJs,NodeJS <br />{" "}
+                    Project Description: Welgo's pharmaceutical dispensing
+                    system allow physicians to dispense prescriptions in-house,
+                    leading to increased patient compliance and satisfaction.{" "}
+                    <br />• Worked with Django to develop REST endpoints for
+                    different services and data. <br />• Worked on integrating
+                    wacom signature device with the frontend. <br />• Integrated
+                    Twilio for sending out notifications. <br />• Used
+                    Websockets for real time notifications. <br /> • Worked with
+                    AngularJs to develop reusable components used across the
+                    product.
                   </p>
                 </div>
               </div>
               <div className="project-item">
                 <div className="picon">
-                <img src={app} alt="app" />;
+                  <img src={app} alt="app" />;
                 </div>
                 <div className="pdesc">
                   <h2>Project#3</h2>
                   <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    Deleniti porro fugiat ducimus incidunt vitae sequi, nemo
-                    aspernatur quo non, provident debitis minima a, odio nisi
-                    laborum! Numquam et omnis aperiam.iat ducimus incidunt vitae
-                    sequi, nemo aspernatur quo non, provident debitis minima a,
-                    odio nisi laborum! Numquam et omnis aperiam.
+                    <b style={{ fontSize: "20px" }}> Measure </b> <br />
+                    Technologies - NodeJS, React, Python, Postgis.
+                    Cloudformation, AWS <br /> Project Description: Everything
+                    you need to automate your operations, fly safely, and
+                    analyze your data with the power of Pix4D. <br />• Worked on
+                    different ETL workflows.
+                    <br /> • Designed and developed various microservices for
+                    handling big data. <br />• Developed a generic map interface
+                    for the frontend with support for multiple visualization
+                    options <br />• Set up automated provisioning and deployment
+                    pipelines for each component in the stack. <br />• Worked
+                    with different satellite/drone imagery datasets for
+                    historical data and predictive analysis.
                   </p>
                 </div>
               </div>
               <div className="project-item">
                 <div className="picon">
-                <img src={app} alt="app" />;
+                  <img src={app} alt="app" />;
                 </div>
                 <div className="pdesc">
                   <h2>Project#4</h2>
                   <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    Deleniti porro fugiat ducimus incidunt vitae sequi, nemo
-                    aspernatur quo non, provident debitis minima a, odio nisi
-                    laborum! Numquam et omnis aperiam.iat ducimus incidunt vitae
-                    sequi, nemo aspernatur quo non, provident debitis minima a,
-                    odio nisi laborum! Numquam et omnis aperiam.
+                    <b style={{ fontSize: "20px" }}> Geospoc </b> <br />• Worked
+                    on different stacks around cloud computing. <br />• Designed and
+                    developed various enterprise grade solutions <br />• Contributed
+                    to inhouse built SaaS and Paas platforms.<br /> • Started as an
+                    individual contributor, currently leading a team of 7
+                    developers.<br /> • Working experience in major phases of product
+                    development like requirement gathering, design, development,
+                    deployment, and unit testing. <br />• Setup CI-CD pipelines for
+                    inhouse products and services.
                   </p>
                 </div>
               </div>
